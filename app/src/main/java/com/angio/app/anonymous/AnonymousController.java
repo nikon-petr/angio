72fc5cad309f0fb42b6878f0a0dda58415a87220
Nikon Petrunin <p.nikwv@gmail.com>
2018-04-23 17:59:49 +0400

    Add matlab jar dependencies

--- app/src/main/java/com/angio/app/anonymous/AnonymousController.java
+++ app/src/main/java/com/angio/app/anonymous/AnonymousController.java
@@ -3,6 +3,11 @@ package com.angio.app.anonymous;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import makeImageBinarization.Class1;
+import com.mathworks.toolbox.javabuilder.MWCharArray;
+import com.mathworks.toolbox.javabuilder.MWException;
+import com.mathworks.toolbox.javabuilder.MWLogicalArray;
+import com.mathworks.toolbox.javabuilder.MWNumericArray;
 
 @RestController
 public class AnonymousController {
@@ -14,6 +19,13 @@ public class AnonymousController {
 
     @RequestMapping("/greeting")
     public AnonymousGreeting greeting() {
-        return new AnonymousGreeting("Greetings for anonymous!");
+        try {
+            Class1 class1 = new Class1();
+            MWCharArray arr = new MWCharArray("https://sun9-2.userapi.com/c840526/v840526794/1841/2RxZYCLMaJU.jpg".toCharArray());
+            Object[] result = class1.makeImageBinarization(1, arr);
+            return new AnonymousGreeting(result.getClass().getSimpleName());
+        } catch (Exception e) {
+            return new AnonymousGreeting(":-(");
+        }
     }
 }
