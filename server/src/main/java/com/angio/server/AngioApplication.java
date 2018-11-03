c81383669daacf39b30f080840a28a67ee2bd173
Nikon Petrunin <p.nikwv@gmail.com>
2018-11-04 00:31:09 +0400

    Integrate log4j2
    1. Add log4j2 configuration
    2. Add usage example in AngioApplication
    3. Add dependencies and cleanup pom.xml
    
    Issue: angio-2

--- server/src/main/java/com/angio/server/AngioApplication.java
+++ server/src/main/java/com/angio/server/AngioApplication.java
@@ -1,6 +1,8 @@
 package com.angio.server;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,12 +10,15 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class AngioApplication {
 
+	private final Logger logger = LoggerFactory.getLogger(AngioApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(AngioApplication.class, args);
 	}
 
 	@Bean
 	public ModelMapper modelMapper() {
+		logger.info("It's started");
 		return new ModelMapper();
 	}
 
