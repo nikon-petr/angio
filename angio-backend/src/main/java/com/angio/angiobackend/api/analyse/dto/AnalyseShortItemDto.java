93d7cd1fffa63f0667982f644d2946ce4e45feb9
Nikon Petrunin <p.nikwv@gmail.com>
2019-04-05 22:45:06 +0400

    Add original image field to AnalyseShortItemDto

--- angio-backend/src/main/java/com/angio/angiobackend/api/analyse/dto/AnalyseShortItemDto.java
+++ angio-backend/src/main/java/com/angio/angiobackend/api/analyse/dto/AnalyseShortItemDto.java
@@ -2,6 +2,8 @@ package com.angio.angiobackend.api.analyse.dto;
 
 import com.angio.angiobackend.api.common.dto.FullNameDto;
 import com.angio.angiobackend.api.patient.dto.PatientShortItemDto;
+import com.angio.angiobackend.api.uploads.dto.StaticFileDto;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,13 +17,33 @@ import java.util.Date;
 @AllArgsConstructor
 public class AnalyseShortItemDto {
 
+    @ApiModelProperty(name = "Analyse id")
     private Long id;
+
+    @ApiModelProperty(name = "Analyse name")
     private String name;
+
+    @ApiModelProperty(name = "Analyse original image")
+    private StaticFileDto originalImage;
+
+    @ApiModelProperty(name = "Analyse short description")
     private String shortDescription;
+
+    @ApiModelProperty(name = "Analyse type")
     private String analyseType;
+
+    @ApiModelProperty(name = "Analyse date")
     private Date analyseDate;
+
+    @ApiModelProperty(name = "Analyse status")
     private AnalyseStatusDto status;
+
+    @ApiModelProperty(name = "Patient data")
     private PatientShortItemDto patient;
+
+    @ApiModelProperty(name = "Diagnostician")
     private FullNameDto diagnostician;
+
+    @ApiModelProperty(name = "Starred property")
     private Boolean starred;
 }
