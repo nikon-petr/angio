cf7eab93a06553622ffaf52e82db59fb80f1ffad
Nikon Petrunin <p.nikwv@gmail.com>
2019-04-24 11:56:48 +0400

    Add home redirect

--- angio-frontend/src/modules/common/components/TheDrawer.vue
+++ angio-frontend/src/modules/common/components/TheDrawer.vue
@@ -20,7 +20,8 @@
                             </v-list-tile-action>
 
                             <v-list-tile-content>
-                                <v-list-tile-title>{{ $t('common.component.theDrawer.link.newAnalyse.name') }}</v-list-tile-title>
+                                <v-list-tile-title>{{ $t('common.component.theDrawer.link.newAnalyse.name') }}
+                                </v-list-tile-title>
                             </v-list-tile-content>
                         </v-list-tile>
                     </template>
@@ -33,13 +34,14 @@
 
                 <v-tooltip right>
                     <template v-slot:activator="{ on }">
-                        <v-list-tile to="/analyse/list" ripple v-on="on" active-class="accent--text">
+                        <v-list-tile to="/analyse" ripple v-on="on" active-class="accent--text">
                             <v-list-tile-action>
                                 <v-icon>search</v-icon>
                             </v-list-tile-action>
 
                             <v-list-tile-content>
-                                <v-list-tile-title>{{ $t('common.component.theDrawer.link.search.name') }}</v-list-tile-title>
+                                <v-list-tile-title>{{ $t('common.component.theDrawer.link.search.name') }}
+                                </v-list-tile-title>
                             </v-list-tile-content>
                         </v-list-tile>
                     </template>
@@ -48,16 +50,17 @@
 
             </PreAuthorize>
 
-            <PreAuthorize v-bind:has-any-of-given-permissions="['USER_VIEW', 'ORGANIZATION_VIEW', 'TOKEN_VIEW', 'ANALYSE_PURGE_DELETED', 'IMAGE_UPLOAD_PURGE_UNUSED']">
+            <PreAuthorize v-bind:has-any-of-given-permissions="dashboardPermissions">
 
                 <v-tooltip right>
                     <template v-slot:activator="{ on }">
-                        <v-list-tile to="/admin/dashboard" ripple v-on="on" active-class="accent--text">
+                        <v-list-tile to="/dashboard" ripple v-on="on" active-class="accent--text">
                             <v-list-tile-action>
                                 <v-icon>dashboard</v-icon>
                             </v-list-tile-action>
 
-                            <v-list-tile-title>{{ $t('common.component.theDrawer.link.dashboard.name') }}</v-list-tile-title>
+                            <v-list-tile-title>{{ $t('common.component.theDrawer.link.dashboard.name') }}
+                            </v-list-tile-title>
                         </v-list-tile>
                     </template>
                     <span>{{ $t('common.component.theDrawer.link.dashboard.tooltip') }}</span>
@@ -69,12 +72,13 @@
 
                 <v-tooltip right>
                     <template v-slot:activator="{ on }">
-                        <v-list-tile to="/admin/user/list" ripple v-on="on" active-class="accent--text">
+                        <v-list-tile to="/user" ripple v-on="on" active-class="accent--text">
                             <v-list-tile-action>
                                 <v-icon>group</v-icon>
                             </v-list-tile-action>
 
-                            <v-list-tile-title>{{ $t('common.component.theDrawer.link.users.name') }}</v-list-tile-title>
+                            <v-list-tile-title>{{ $t('common.component.theDrawer.link.users.name') }}
+                            </v-list-tile-title>
                         </v-list-tile>
                     </template>
                     <span>{{ $t('common.component.theDrawer.link.users.tooltip') }}</span>
@@ -86,12 +90,13 @@
 
                 <v-tooltip right>
                     <template v-slot:activator="{ on }">
-                        <v-list-tile to="/admin/organization/list" ripple v-on="on" active-class="accent--text">
+                        <v-list-tile to="/organization" ripple v-on="on" active-class="accent--text">
                             <v-list-tile-action>
                                 <v-icon>domain</v-icon>
                             </v-list-tile-action>
 
-                            <v-list-tile-title>{{ $t('common.component.theDrawer.link.organizations.name') }}</v-list-tile-title>
+                            <v-list-tile-title>{{ $t('common.component.theDrawer.link.organizations.name') }}
+                            </v-list-tile-title>
                         </v-list-tile>
                     </template>
                     <span>{{ $t('common.component.theDrawer.link.organizations.tooltip') }}</span>
@@ -103,12 +108,13 @@
 
                 <v-tooltip right>
                     <template v-slot:activator="{ on }">
-                        <v-list-tile to="/admin/organization/list" ripple v-on="on" active-class="accent--text">
+                        <v-list-tile to="/notification/new" ripple v-on="on" active-class="accent--text">
                             <v-list-tile-action>
                                 <v-icon>add_alert</v-icon>
                             </v-list-tile-action>
 
-                            <v-list-tile-title>{{ $t('common.component.theDrawer.link.newNotification.name') }}</v-list-tile-title>
+                            <v-list-tile-title>{{ $t('common.component.theDrawer.link.newNotification.name') }}
+                            </v-list-tile-title>
                         </v-list-tile>
                     </template>
                     <span>{{ $t('common.component.theDrawer.link.newNotification.tooltip') }}</span>
@@ -145,6 +151,7 @@
                 </v-tooltip>
 
             </PreAuthorize>
+
         </v-list>
     </v-navigation-drawer>
 </template>
@@ -153,11 +160,20 @@
     import {Component, Vue} from 'vue-property-decorator';
     import PreAuthorize from '@/modules/common/components/PreAuthorize.vue';
     import {userModule} from '@/modules/user/store/userStore';
+    import {UserPermission} from "@/modules/user/store/userState";
 
     @Component({
         components: {PreAuthorize}
     })
     export default class TheDrawer extends Vue {
+
+        public readonly dashboardPermissions: UserPermission[] = [
+            UserPermission.USER_VIEW,
+            UserPermission.ORGANIZATION_VIEW,
+            UserPermission.TOKEN_VIEW,
+            UserPermission.ANALYSE_PURGE_DELETED,
+            UserPermission.IMAGE_UPLOAD_PURGE_UNUSED
+        ];
 
         @userModule.Getter
         public readonly isAuthenticated!: boolean;
--- angio-frontend/src/router.ts
+++ angio-frontend/src/router.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
-import Router, {RouteConfig} from 'vue-router';
+import root, {Logger} from 'loglevel';
+import Router, {Route, RouteConfig} from 'vue-router';
 import {AuthPredicate} from '@/modules/common/helpers/authPredicate';
 import {userRouterConfig} from '@/modules/user/userRouter';
 import NotFound from '@/modules/common/views/404.vue';
@@ -8,10 +9,15 @@ import ServerError from '@/modules/common/views/500.vue';
 import i18n from '@/plugins/i18n';
 import About from '@/modules/common/views/About.vue';
 import {analyseRouterConfig} from '@/modules/analyse/analyseRouter';
+import {hasAnyOfGivenPermissions, hasPermissions} from '@/modules/user/store/userStore';
+import {UserPermission} from '@/modules/user/store/userState';
+
+const log: Logger = root.getLogger('router');
 
 Vue.use(Router);
 
 let rootRouterConfig: RouteConfig[] = [
+    {path: '/', alias: '/home', redirect: homeRedirect},
     {path: '/about', component: About, meta: {title: 'common.view.about.title', auth: AuthPredicate.permitAll()}},
     {path: '/500', component: ServerError, meta: {title: 'common.view.500.title', auth: AuthPredicate.permitAll()}},
     {path: '/404', component: NotFound, meta: {title: 'common.view.404.title', auth: AuthPredicate.permitAll()}},
@@ -45,5 +51,25 @@ store.watch(((state, getters) => getters['user/isAuthenticated']), (newValue, ol
         rootRouter.push({path: '/login'});
     }
 });
+
+// home redirect
+function homeRedirect (to: Route) {
+    if(hasPermissions(store)([UserPermission.ANALYSE_VIEW])) {
+        log.debug('redirect to /analyse');
+        return {path: '/analyse', replace: true};
+    } else if (hasAnyOfGivenPermissions(store)([
+        UserPermission.USER_VIEW,
+        UserPermission.ORGANIZATION_VIEW,
+        UserPermission.TOKEN_VIEW,
+        UserPermission.ANALYSE_PURGE_DELETED,
+        UserPermission.IMAGE_UPLOAD_PURGE_UNUSED
+    ])) {
+        log.debug('redirect to /dashboard');
+        return {path: '/dashboard', replace: true};
+    } else {
+        log.debug('redirect to /landing');
+        return {path: '/landing', replace: true};
+    }
+}
 
 export default rootRouter;
