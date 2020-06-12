f631f87ce0d1d72a2546215e4afadc8ddeedac33
Artyom Karkavin <touchitteam@gmail.com>
2020-06-12 13:50:36 +0400

    Display error from server for user forms
    Resolve GH-78

--- angio-frontend/src/modules/user/views/Registration.vue
+++ angio-frontend/src/modules/user/views/Registration.vue
@@ -40,7 +40,11 @@
                     this.$router.push({path: '/login'});
                 })
                 .catch((error) => {
-                    this.errorMessages = ['user.component.registrationForm.error'];
+                    if (error.response.status >= 400 && error.response.status < 500) {
+                        this.errorMessages = [error.response.data.error.message];
+                    } else if (error.response.status >= 500) {
+                        this.errorMessages = ['common.form.error.500'];
+                    }
                     this.$logger.error(error)
                 })
                 .finally(() => this.fetching = false)
